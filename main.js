35e0820c5c2a9fe1525ce49629bf2c54ef76f741
tzlilhaim <tzlilhaim480@gmail.com>
2020-08-25 16:47:10 +0300

    added classes for OOP office exercise

--- main.js
+++ main.js
@@ -1,2 +1,56 @@
 //write your code here to make the tests pass
+class Document {
+  constructor(name) {
+    this.EmployeeName = name
+  }
+}
 
+class Employee {
+  constructor(name) {
+    this.name = name
+  }
+  work(office) {
+    for (let i = 0; i < 10; i++) {
+      office.documents.push(new Document(this.name))
+    }
+  }
+}
+
+class Manager {
+  constructor(name) {
+    this.name = name
+    this.employees = []
+  }
+  hireEmployee(name) {
+    this.employees.push(new Employee(name))
+  }
+  askEmployeesToWork(office) {
+    this.employees.forEach(e => e.work(office))
+  }
+}
+
+class Cleaner {
+  constructor(name) {
+    this.name = name
+  }
+  clean() {
+    console.log("Clean")
+  }
+}
+
+class Office {
+  constructor() {
+    this.documents = []
+    this.managers = []
+    this.cleaners = []
+  }
+  hireCleaner(name) {
+    this.cleaners.push(new Cleaner(name))
+  }
+  hireManager(name) {
+    this.managers.push(new Manager(name))
+  }
+  startWorkDay() {
+    this.managers.forEach((m) => m.askEmployeesToWork(this))
+  }
+}
